7795a2e754be50dcf965493b4ea24e7bbeeef37e
sajjad1hussain <74602484+sajjad1hussain@users.noreply.github.com>
2025-02-16 18:36:16 +0500

    updated

--- web_hooks_community/models/message.py
+++ web_hooks_community/models/message.py
@@ -6,15 +6,18 @@ import json
 class MailMessage(models.Model):
     _inherit = 'mail.message'
 
+    is_through_integration = fields.Boolean()
+
+
     def send_community_chatter_data(self):
         for rec in self:
             # url = "http://8.213.43.22:8074/web/hook/5d30af0e-4a3e-4683-9243-b424c6080913"
             url = self.env.company.odoo_chatter_automation_url
             if url:
                 payload = json.dumps({
-                    "res_id": 25,
+                    "res_id": 8,
                     "model": rec.model,
-                    "body": rec.body
+                    "body": rec.body,
                 })
                 headers = {
                     'Content-Type': 'application/json'
@@ -26,7 +29,8 @@ class MailMessage(models.Model):
     @api.model
     def create(self, values):
         res = super(MailMessage, self).create(values)
-        res.send_community_chatter_data()
+        if not res.is_through_integration:
+            res.send_community_chatter_data()
         return res
 
 
@@ -35,5 +39,6 @@ class MailMessage(models.Model):
         self.create({
             'res_id': payload.get('res_id'),
             'model': payload.get('model'),
-            'body': payload.get('body')
+            'body': payload.get('body'),
+            'is_through_integration': True
         })
