3ea603ee1eb1bc4be7e0403ccd987a465f7ec7bf
sajjad1hussain <74602484+sajjad1hussain@users.noreply.github.com>
2025-02-19 01:15:29 +0500

    updated

--- web_hooks_community/models/attachment.py
+++ web_hooks_community/models/attachment.py
@@ -2,7 +2,7 @@
 from odoo import api, fields, models, SUPERUSER_ID, tools, _
 from odoo.addons.base.models.ir_attachment import IrAttachment as BaseIrAttachment
 import base64
-
+import json
 
 
 
@@ -11,34 +11,10 @@ class IrAttachment(models.Model):
 
     is_through_integration = fields.Boolean()
 
-
-@api.model_create_multi
-def create(self, vals_lists):
-    for vals_list in vals_lists:
-        if vals_list[0].get('is_through_integration', False):
-            return super().create(vals_list)
-        else:
-            record_tuple_set = set()
-            vals_list = [{
-                key: value
-                for key, value
-                in vals.items()
-                if key not in ('file_size', 'checksum', 'store_fname')
-            } for vals in vals_list]
-            for values in vals_list:
-                values = self._check_contents(values)
-                raw, datas = values.pop('raw', None), values.pop('datas', None)
-                if raw or datas:
-                    if isinstance(raw, str):
-                        raw = raw.encode()
-                    values.update(self._get_datas_related_values(
-                        raw or base64.b64decode(datas or b''),
-                        values['mimetype']
-                    ))
-                record_tuple = (values.get('res_model'), values.get('res_id'))
-                record_tuple_set.add(record_tuple)
-            Attachments = self.browse()
-            for res_model, res_id in record_tuple_set:
-                Attachments.check('create', values={'res_model':res_model, 'res_id':res_id})
-            return super(BaseIrAttachment, self).create(vals_list)
-BaseIrAttachment.create = IrAttachment.create
+    def create_data(self, payload):
+        self.env['ir.attachment'].sudo().create({
+            'name': payload.get('name'),
+            'store_fname': payload.get('store_fname'),
+            'mimetype': payload.get('mimetype'),
+            'datas': base64.b64decode(payload.get('datas')),
+        })
