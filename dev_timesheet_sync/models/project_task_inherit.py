e3585af7f1f70d96c3796cdcadf30b5ebda40a52
Muhammad Imran Sarwar <mmanisarwar@gmail.com>
2025-02-20 16:12:56 +0500

    ddd

--- dev_timesheet_sync/models/project_task_inherit.py
+++ dev_timesheet_sync/models/project_task_inherit.py
@@ -12,18 +12,18 @@ class ProjectTask(models.Model):
         """
 
         # sync_url = self.env.company.sync_url
-        sync_url = 'https://art-ethereal-advertising-company-staging-main-18283947.dev.odoo.com'
-        # sync_db = self.env.company.sync_db
-        sync_db = 'art-ethereal-advertising-company-staging-main-18283947'
-        # sync_login = self.env.company.sync_login
-        sync_login = 'admin@gmail.com'
-        # sync_pass = self.env.company.sync_pass
-        sync_pass = 'admin'
+        # sync_url = 'https://art-ethereal-advertising-company-staging-main-18283947.dev.odoo.com'
+        # # sync_db = self.env.company.sync_db
+        # sync_db = 'art-ethereal-advertising-company-staging-main-18283947'
+        # # sync_login = self.env.company.sync_login
+        # sync_login = 'admin@gmail.com'
+        # # sync_pass = self.env.company.sync_pass
+        # sync_pass = 'admin'
 
-        # sync_url = self.env.company.sync_url
-        # sync_db = self.env.company.sync_db
-        # sync_login = self.env.company.sync_login
-        # sync_pass = self.env.company.sync_pass
+        sync_url = self.env.company.sync_url
+        sync_db = self.env.company.sync_db
+        sync_login = self.env.company.sync_login
+        sync_pass = self.env.company.sync_pass
 
         enterprise_config = {
             'url': sync_url,
@@ -59,9 +59,11 @@ class ProjectTask(models.Model):
 
         # Create or update task in the Enterprise Odoo instance
         try:
-            sync_db = 'art-ethereal-advertising-company-staging-main-18283947'
-            # sync_pass = self.env.company.sync_pass
-            sync_pass = 'admin'
+            # sync_db = 'art-ethereal-advertising-company-staging-main-18283947'
+            sync_db = self.env.company.sync_db
+
+            sync_pass = self.env.company.sync_pass
+            # sync_pass = 'admin'
 
             enterprise_task_id = enterprise_models.execute_kw(
                 sync_db, enterprise_uid, sync_pass,
